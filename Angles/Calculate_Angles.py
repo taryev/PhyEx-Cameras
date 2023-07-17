1c3297f6d53525ae9c2c86f111564adf9a07aadc
MihailS <soporanmihail@gmai;.com>
2023-07-17 13:29:38 +0200

    Added filter, functional for *.npy files

--- Angles/Calculate_Angles.py
+++ Angles/Calculate_Angles.py
@@ -1,166 +1,202 @@
-import pandas as pd
+
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy import signal
+import glob
+import os
+# Set the folder path where the files are located
+
+folder_path = 'C:\\Users\\MihailS\\Documents\\Project\\Test\\Openpose\\TestSample'
+
+# Get a list of all XLSX and CSV files in the folder
+files = glob.glob(folder_path + '/*.npy')
+
+# Process each file
+for file in files:
+    data1 = np.load(file)
 
 
-def read_angles_csvs(csv1 : str,  joint1 : int, joint2 : int, joint3 : int, csv2 : str,joint4 : int, joint5 : int, joint6 : int):
-    
-    #Read the CSV
-    data1 = pd.read_csv(csv1, header=None)
-    data2 = pd.read_csv(csv2, header=None)
+    def read_angle_for_1_csv(joint1: int, joint2: int, joint3: int, joint4: int, joint5: int, joint6: int,
+                             angle_to_analyse: int):
+        file_name = os.path.basename(file)
+        angles1 = []
+        angles2 = [] #add as many vectors as you want, add 3 joints per each in the function
 
-    angles1 = []
-    angles2 = []
+        num_rows1 = data1.shape[0]
 
-    num_rows1, _ = data1.shape
-    num_rows2, _ = data2.shape
-    
+        for j in range(num_rows1):
+            try:
+                x_a1 = data1[j, joint1,0] #x,y,z dimensions since the data1 shape is [n,25,3]
+                y_a1 = data1[j, joint1,1]
 
-    for j in range (max(num_rows1 , num_rows2)):
-        
-        if(j in data1.index and j in data2.index):
-            #We get the coordinates for each csv
-            x_a = data1.iloc[j,3 * joint1] 
-            y_a = data1.iloc[j,3 * joint1 + 1]
+                x_b1 = data1[j, joint2,0]
+                y_b1 = data1[j, joint2,1]
 
-            x_a_2 = data2.iloc[j,3 * joint4] 
-            y_a_2 = data2.iloc[j,3 * joint4 + 1]
+                x_c1 = data1[j, joint3,0]
+                y_c1 = data1[j, joint3,1]
 
-            x_b = data1.iloc[j,3 * joint2] 
-            y_b = data1.iloc[j,3 * joint2 + 1]
+                x_a2 = data1[j, joint4,0]
+                y_a2 = data1[j, joint4,1]
 
-            x_b_2 = data2.iloc[j,3 * joint5] 
-            y_b_2 = data2.iloc[j,3 * joint5 + 1]
+                x_b2 = data1[j, joint5,0]
+                y_b2 = data1[j, joint5,1]
 
-            x_c = data1.iloc[j,3 * joint3] 
-            y_c = data1.iloc[j,3 * joint3 + 1]
+                x_c2 = data1[j, joint6,0]
+                y_c2 = data1[j, joint6,1]
 
-            x_c_2 = data2.iloc[j,3 * joint6] 
-            y_c_2 = data2.iloc[j,3 * joint6 + 1]
+                if np.all(x_b1 != x_a1):
+                    AB1 = (y_b1 - y_a1) / (x_b1 - x_a1)
+                else:
+                    AB1 = 0.0
 
-            #We calculate the distance for each csv
+                if np.all(x_b1 != x_c1):
+                    BC1 = (y_c1 - y_b1) / (x_c1 - x_b1)
+                else:
+                    BC1 = 0.0
 
-            AB = (y_b - y_a)/(x_b - x_a)
-            BC = (y_c - y_b)/(x_c - x_b)
+                if np.all(x_b2 != x_a2):
+                    AB2 = (y_b2 - y_a2) / (x_b2 - x_a2)
+                else:
+                    AB2 = 0.0
 
-            AB_2 = (y_b_2 - y_a_2)/(x_b_2 - x_a_2)
-            BC_2 = (y_c_2 - y_b_2)/(x_c_2 - x_b_2)
+                if np.all(x_b2 != x_c2):
+                    BC2 = (y_c2 - y_b2) / (x_c2 - x_b2)
+                else:
+                    BC2 = 0.0
 
-            #We calculate and convert the angle for each csv
+                radians1 = np.arctan2((AB1 - BC1), (1 + AB1 * BC1))
+                angle1 = 180 - np.abs(radians1 * 180.0 / np.pi)
+                radians2 = np.arctan2((AB2 - BC2), (1 + AB2 * BC2))
+                angle2 = 180 - np.abs(radians2 * 180.0 / np.pi)
 
-            radians1 = np.arctan2((AB - BC), (1 + AB * BC))
-            radians2 = np.arctan2((AB_2 - BC_2), (1 + AB_2 * BC_2))
+                angles1.append(angle1)
+                angles2.append(angle2)
+            except (ZeroDivisionError, RuntimeWarning):
+                angles1.append(np.nan)
+                angles2.append(np.nan)
 
-            angle1 = 180 - np.abs(radians1*180.0/np.pi)
-            angle2 = 180 - np.abs(radians2*180.0/np.pi)
-    
-            angles1.append(angle1)
-            angles2.append(angle2)
-        
-        elif j in data1.index:
+        angles1 = np.asarray(angles1)
+        angles2 = np.asarray(angles2)
 
-            x_a = data1.iloc[j,3 * joint1] 
-            y_a = data1.iloc[j,3 * joint1 + 1]
+        inf_interval = 0.8 * angle_to_analyse
+        sup_interval = 1.2 * angle_to_analyse
 
-            x_b = data1.iloc[j,3 * joint2] 
-            y_b = data1.iloc[j,3 * joint2 + 1]
+        valid_angles1 = np.logical_and(np.logical_not(np.isnan(angles1)),
+                                       np.logical_and(angles1 >= inf_interval, angles1 <= sup_interval))
+        valid_angles2 = np.logical_and(np.logical_not(np.isnan(angles2)),
+                                       np.logical_and(angles2 >= inf_interval, angles2 <= sup_interval))
 
-            x_c = data1.iloc[j,3 * joint3] 
-            y_c = data1.iloc[j,3 * joint3 + 1]
+        count = np.count_nonzero(valid_angles1) + np.count_nonzero(valid_angles2)
+        print('angle is respected', (count / num_rows1) * 100)
 
-            AB = (y_b - y_a)/(x_b - x_a)
-            BC = (y_c - y_b)/(x_c - x_b)
+        N1 = 150  # window length
+        w1 = 4  # polyorders
+    #Savintsky-Golay filter
+        angles_filt1 = signal.savgol_filter(angles1, N1, w1)
+        angles_filt2 = signal.savgol_filter(angles2, N1, w1)
 
-            radians1 = np.arctan2((AB - BC), (1 + AB * BC))
-            angle1 = 180 - np.abs(radians1*180.0/np.pi)
-            angles1.append(angle1)
+        plt.figure()
+        plt.subplot(121)
+        plt.plot(angles1, color='purple')
+        plt.plot(angles_filt1, color='orange')
+        plt.title("Knee")
 
-        elif j in data2.index:
+        plt.subplot(122)
+        plt.plot(angles2, color='blue')
+        plt.plot(angles_filt2, color='red')
+        plt.title("Elbow")
 
-            x_a_2 = data2.iloc[j,3 * joint4] 
-            y_a_2 = data2.iloc[j,3 * joint4 + 1]
+        plt.suptitle(file_name)
+        plt.tight_layout()
+        plt.show()
 
-            x_b_2 = data2.iloc[j,3 * joint5] 
-            y_b_2 = data2.iloc[j,3 * joint5 + 1]
-
-            x_c_2 = data2.iloc[j,3 * joint6] 
-            y_c_2 = data2.iloc[j,3 * joint6 + 1]
-
-            AB_2 = (y_b_2 - y_a_2)/(x_b_2 - x_a_2)
-            BC_2 = (y_c_2 - y_b_2)/(x_c_2 - x_b_2)
-
-            radians2 = np.arctan2((AB_2 - BC_2), (1 + AB_2 * BC_2))
-            angle2 = 180 - np.abs(radians2*180.0/np.pi)
-            angles2.append(angle2)
-
-
-    fig = plt.figure()
-
-    ax1 = plt.subplot(211)
-    ax1.plot(angles1, color='purple')
-    ax1.set_ylabel('Angles')
-    ax1.set_title('Angles from the first CSV')
-
-    ax2 = plt.subplot(212)
-    ax2.plot(angles2, color='orange')
-    ax2.set_ylabel('Angles')
-    ax2.set_title('Angles from the second CSV')
-
-    #Automatically adjust the spacing between subplots
-    plt.tight_layout()
-    plt.show()
+        return angles1, angles_filt1, angles2, angles_filt2, file_name, num_rows1
 
 
-#enter the CSV file and the number of the joints which form the ABC triangle 
-read_angles_csvs('C:\\Users\\33770\\Documents\\Stage_2A\\1LBR_Victor.csv', 24, 26, 28, 'C:\\Users\\33770\\Documents\\Stage_2A\\1LBR_Physio.csv', 24, 26, 28)
+    #save the angles and filters of each file
+    a1, af1, a2, af2, file_name, num_rows1 = read_angle_for_1_csv(9, 10, 11, 2, 3, 4, 120)
+    #cost function
+    def dtw_distance(angles1, angles2):
+        m = len(angles1)
+        n = len(angles2)
 
-def read_angle_for_1_csv(csv1 : str,  joint1 : int, joint2 : int, joint3 : int, angle_to_analyse : int):
-    
-    data1 = pd.read_csv(csv1, header=None)
+        # Create a cost matrix
+        cost_matrix = np.zeros((m, n))
 
-    angles1 = []
+      # Initialise the first row and column of the cost matrix
 
-    num_rows1, _ = data1.shape
+        cost_matrix[0, 0] = abs(angles1[0] - angles2[0])
+        for i in range(1, m):
+            cost_matrix[i, 0] = cost_matrix[i-1, 0] + abs(angles1[i] - angles2[0])
+        for j in range(1, n):
+            cost_matrix[0, j] = cost_matrix[0, j-1] + abs(angles1[0] - angles2[j])
 
-    for j in range (0, num_rows1):
-        
-        x_a = data1.iloc[j,3 * joint1] 
-        y_a = data1.iloc[j,3 * joint1 + 1]
+        # Fill in the rest of the cost matrix
+        for i in range(1, m):
+            for j in range(1, n):
+                cost_matrix[i, j] = abs(angles1[i] - angles2[j]) + min(cost_matrix[i - 1, j], cost_matrix[i, j - 1],
+                                                                       cost_matrix[i - 1, j - 1])
 
-        x_b = data1.iloc[j,3 * joint2] 
-        y_b = data1.iloc[j,3 * joint2 + 1]
+            # Calculate the distance
+        cost = cost_matrix[m - 1, n - 1]
 
-        x_c = data1.iloc[j,3 * joint3] 
-        y_c = data1.iloc[j,3 * joint3 + 1]
+        return cost
 
-        AB = (y_b - y_a)/(x_b - x_a)
-        BC = (y_c - y_b)/(x_c - x_b)
-
-        radians1 = np.arctan2((AB - BC), (1 + AB * BC))
-        angle1 = 180 - np.abs(radians1*180.0/np.pi)
-        
-        angles1.append(angle1)
+    cost1 = dtw_distance(a1, af1)
+    cost2 = dtw_distance(a2, af2)
 
 
-    inf_interval = 0.8*angle_to_analyse
-    sup_interval = 1.2*angle_to_analyse
-    count=0
-    
-    for angle in angles1:
-        if (angle>=inf_interval) and (angle<=sup_interval):
-            count+=1
-            print(angle)
-            print("Respected angle")
+    print(file_name, "Distance DTW Knee:", cost1)
+    print(file_name, "Distance DTW Elbow:", cost2)
+    #enter the CSV file, the number of the joints which form the ABC triangle, and the angle you want to check
+    # save the data in the folder
+    output_dir = os.path.dirname(file)
+    base_name = os.path.splitext(file_name)[0]
+    output_file = os.path.join(output_dir, f'{base_name}_angle_results.txt')
 
-    print('angle is respected', (count/num_rows1)*100)
-
+    # Output the results to the individualized text file
+    angle_to_analyse=120
+    inf = 0.8 * angle_to_analyse
+    sup = 1.2 * angle_to_analyse
+    count = 0
+    with open(output_file, 'w') as f:
+        for angle in a1:
+            if (angle >= inf) and (angle <= sup):
+                count += 1
+                f.write(f'{angle}\n')
+                f.write("Respected knee angle\n")
+        f.write(f'angle is respected {(count / num_rows1) * 100}\n')
+        count=0
+        for angle in a2:
+            if (angle >= inf) and (angle <= sup):
+                count += 1
+                f.write(f'{angle}\n')
+                f.write("Respected elbow angle\n")
+        f.write(f'angle is respected {(count / num_rows1) * 100}\n')
+        f.write(f'Distance DTW knee: {cost1}\n')
+        f.write(f'Distance DTW elbow: {cost2}\n')
+    #saving the figure
     plt.figure()
-    plt.plot(angles1, color='purple')
-    plt.title('Angles from the first CSV')
 
-    
-    plt.show()
+    plt.subplot(121)
+    plt.plot(a1, color='purple')
+    plt.plot(af1, color='orange')
+    plt.title("Knee")
 
+    plt.subplot(122)
+    plt.plot(a2, color='blue')
+    plt.plot(af2, color='red')
+    plt.title("Elbow")
 
-#enter the CSV file, the number of the joints which form the ABC triangle, and the angle you want to check
-read_angle_for_1_csv('C:\\Users\\33770\\Documents\\Stage_2A\\1LBR_Victor.csv', 24, 26, 28, 120)
+    plt.tight_layout()
+    plt.suptitle(file_name)
+
+    plt.savefig(os.path.join(output_dir, f'{base_name}_plot.png'))
+
+#    def data_type(file:str, joint1:int):
+#        file_name= os.path.basename(file)
+#        shape=data1.shape
+#        return file_name,shape
+#    name,shape=data_type(file,3)
+#    print(name,' ',shape)
+    #enter the CSV file and the number of the joints which form the ABC triangle
